e52966190395b5a5679dfd3737ba5d401453c6f9
Deriech <deriech73@gmail.com>
2018-01-30 03:40:37 -0500

    1st Finished Iteration of Bemuse Bot

--- BemuseBot/src/BemuseBot/BemuseBot.java
+++ BemuseBot/src/BemuseBot/BemuseBot.java
@@ -25,23 +25,28 @@ public class BemuseBot {
     public static void main(String[] args) throws AWTException, InterruptedException, IOException {
         Robot robot = new Robot();
         while (true) {
-            boolean PressButton = quickTime(532, 575, robot);
-            if (PressButton) {
-                robot.keyPress(KeyEvent.VK_S);
-            } else {
-                robot.keyRelease(KeyEvent.VK_S);
-            }
+        quickTime(532, 575, robot, KeyEvent.VK_S);
+        quickTime(580, 575, robot, KeyEvent.VK_D);
+        quickTime(620, 575, robot, KeyEvent.VK_F);
+        quickTime(665, 575, robot, KeyEvent.VK_SPACE);
+        quickTime(710, 575, robot, KeyEvent.VK_J);
+        quickTime(755, 575, robot, KeyEvent.VK_K);
+        quickTime(800, 575, robot, KeyEvent.VK_L);
+            
         }
         // TODO code application logic here
     }
 
-    public static boolean quickTime(int XCoord, int YCoord, Robot robot) {
-        Rectangle rectArea = new Rectangle(XCoord, YCoord, 20, 20);
-        BufferedImage screenFullImage = robot.createScreenCapture(rectArea);
-        //ImageIO.write(screenFullImage, "jpg", new File("test.jpg"));
-        Color RGB = new Color(screenFullImage.getRGB(5, 2));
+    public static void quickTime(int XCoord, int YCoord, Robot robot, int key) {
+        BufferedImage screenFullImage = robot.createScreenCapture(new Rectangle(XCoord, YCoord, 10, 10));
+        Color RGB = new Color(screenFullImage.getRGB(3, 0));
         int red = RGB.getBlue();
-        return red > 200;
+        if (red >180) {
+                robot.keyPress(key);
+          } else {
+                robot.keyRelease(key);
+                
+            }
     }
 
 }
