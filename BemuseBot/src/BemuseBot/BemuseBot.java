f6fbc7381432451cceae750345e936717aace487
Deriech <deriech73@gmail.com>
2018-01-30 00:42:24 -0500

    Created Senitivity POC

--- BemuseBot/src/BemuseBot/BemuseBot.java
+++ BemuseBot/src/BemuseBot/BemuseBot.java
@@ -6,13 +6,16 @@
 package BemuseBot;
 
 import java.awt.AWTException;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -28,31 +31,26 @@ public class BemuseBot {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws AWTException, InterruptedException {
-        try {
-            KeyEvent keys;
-            Robot temmy = new Robot();
-            boolean switchRobot = true;
-            
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            System.out.println(screenSize.getHeight());
-            System.out.println(screenSize.getWidth());
-            Robot robot = new Robot();
-            File test = new File("Test2.png");
-            
+    public static void main(String[] args) throws AWTException, InterruptedException, IOException {
+          Robot robot = new Robot();
+
         
             while(true){
-                
-                 Rectangle rectArea = new Rectangle(532,573,300,200);
+            Rectangle rectArea = new Rectangle(532,575,20,20);
             BufferedImage screenFullImage = robot.createScreenCapture(rectArea);
-            ImageIO.write(screenFullImage, "png", new File("Test2.png"));
-            robot.delay(5);
+            //ImageIO.write(screenFullImage, "jpg", new File("test.jpg"));
+            Color RGB = new Color(screenFullImage.getRGB(3, 3));
+            int red = RGB.getBlue();
+            if(red > 200){
+            System.out.println(red);
+            }
             }
             
-            // TODO code application logic here
-        } catch (IOException ex) {
-            Logger.getLogger(BemuseBot.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            
+        
+        
+        
+        // TODO code application logic here
     }
 
 }
