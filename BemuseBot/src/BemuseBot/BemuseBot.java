ff340390b981b79d7ee62be9640db84515df5575
Deriech <deriech73@gmail.com>
2018-01-31 16:30:47 -0500

    Improved efficiency by making one screenshot, instead of 7

--- BemuseBot/src/BemuseBot/BemuseBot.java
+++ BemuseBot/src/BemuseBot/BemuseBot.java
@@ -24,30 +24,31 @@ public class BemuseBot {
      */
     public static void main(String[] args) throws AWTException, InterruptedException, IOException {
         Robot robot = new Robot();
-        final int yCoord = 573;
+        //ImageIO.write(screenFullImage, "png", new File("Test2.png"));
+        final int yCoord = 0;
         while (true) {
-        quickTime(532, yCoord, robot, KeyEvent.VK_S);
-        quickTime(580, yCoord, robot, KeyEvent.VK_D);
-        quickTime(620, yCoord, robot, KeyEvent.VK_F);
-        quickTime(665, yCoord, robot, KeyEvent.VK_SPACE);
-        quickTime(710, yCoord, robot, KeyEvent.VK_J);
-        quickTime(755, yCoord, robot, KeyEvent.VK_K);
-        quickTime(800, yCoord, robot, KeyEvent.VK_L);
-            
+            BufferedImage screenFullImage = robot.createScreenCapture(new Rectangle(532, 582, 301, 14));
+            quickTime(2, yCoord, robot, KeyEvent.VK_S, screenFullImage);
+            quickTime(46, yCoord, robot, KeyEvent.VK_D, screenFullImage);
+            quickTime(86, yCoord, robot, KeyEvent.VK_F, screenFullImage);
+            quickTime(131, yCoord, robot, KeyEvent.VK_SPACE, screenFullImage);
+            quickTime(176, yCoord, robot, KeyEvent.VK_J, screenFullImage);
+            quickTime(221, yCoord, robot, KeyEvent.VK_K, screenFullImage);
+            quickTime(261, yCoord, robot, KeyEvent.VK_L, screenFullImage);
+
         }
         // TODO code application logic here
     }
 
-    public static void quickTime(int XCoord, int YCoord, Robot robot, int key) {
-        BufferedImage screenFullImage = robot.createScreenCapture(new Rectangle(XCoord, YCoord, 10, 10));
-        Color RGB = new Color(screenFullImage.getRGB(3, 0));
+    public static void quickTime(int XCoord, int YCoord, Robot robot, int key, BufferedImage image) throws IOException {
+        Color RGB = new Color(image.getRGB(XCoord, YCoord));
         int red = RGB.getBlue();
-        if (red >180) {
-                robot.keyPress(key);
-          } else {
-                robot.keyRelease(key);
-                
-            }
+        if (red > 180) {
+            robot.keyPress(key);
+        } else {
+            robot.keyRelease(key);
+
+        }
     }
 
 }
