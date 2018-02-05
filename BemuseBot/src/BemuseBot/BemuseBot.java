7edaf70e7a3b50c518336f904dded54a5a177e2a
Deriech <deriech73@gmail.com>
2018-02-05 12:47:05 -0500

    Made efficiency improvements

--- BemuseBot/src/BemuseBot/BemuseBot.java
+++ BemuseBot/src/BemuseBot/BemuseBot.java
@@ -11,9 +11,7 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import javax.imageio.ImageIO;
 
 /**
  *
@@ -27,28 +25,32 @@ public class BemuseBot {
     public static void main(String[] args) throws AWTException, InterruptedException, IOException {
         Robot robot = new Robot();
         //ImageIO.write(screenFullImage, "png", new File("Test2.png"));
-        final int yCoord = 0;
+        final int yCoord = 4;
+        final int lessSensitive = -6000000;
+        final int MoreSensitive = -9000000;
         while (true) {
-            
+
             BufferedImage pic = robot.createScreenCapture(new Rectangle(532, 573, 301, 14));
-            quickTime(2, yCoord, robot, KeyEvent.VK_S, pic);
-            quickTime(46, yCoord , robot, KeyEvent.VK_D, pic);
-            quickTime(86, yCoord , robot, KeyEvent.VK_F, pic);
-            quickTime(131, yCoord , robot, KeyEvent.VK_SPACE, pic);
-            quickTime(176, yCoord , robot, KeyEvent.VK_J, pic);
-            quickTime(221, yCoord , robot, KeyEvent.VK_K, pic);
-            quickTime(261, yCoord , robot, KeyEvent.VK_L, pic);
+            quickTime(2, yCoord, robot, KeyEvent.VK_S, pic, lessSensitive);
+            quickTime(46, yCoord + 2, robot, KeyEvent.VK_D, pic, MoreSensitive);
+            quickTime(86, yCoord, robot, KeyEvent.VK_F, pic, lessSensitive);
+            quickTime(131, yCoord + 2, robot, KeyEvent.VK_SPACE, pic, MoreSensitive);
+            quickTime(176, yCoord, robot, KeyEvent.VK_J, pic, lessSensitive);
+            quickTime(221, yCoord + 2, robot, KeyEvent.VK_K, pic, MoreSensitive);
+            quickTime(261, yCoord, robot, KeyEvent.VK_L, pic, lessSensitive);
 
         }
         // TODO code application logic here
     }
 
-    public static void quickTime(int XCoord, int YCoord, Robot robot, int key, BufferedImage image) throws IOException {
-        int red = new Color(image.getRGB(XCoord, YCoord)).getBlue();
-        if (red > 180) {
+    public static void quickTime(int XCoord, int YCoord, Robot robot, int key, BufferedImage image, int ARGB) throws IOException {
+        
+        if (image.getRGB(XCoord, YCoord) > ARGB) {
             robot.keyPress(key);
+            
         } else {
             robot.keyRelease(key);
+            
 
         }
     }
